d51395a573f46f39544ca6cd60f5b683b5a57452
rdcotrina <roger.cotrina.c@gmail.com>
2017-07-03 20:29:55 -0500

    2
    2

--- js/optimizer.jquery.js
+++ js/optimizer.jquery.js
@@ -1,7 +1,44 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function($){
+    
+    $.fn.extend({
+        
+        optimizer: function(opt){
+            
+            let defaults = {
+                data: []
+            };
 
+            var options = $.extend(defaults, opt);
+            
+            /*=========================================METODOS PRIVADOS=========================================*/
+            var _private = {
+                
+                render: function(oSettings){
+                    alert(this.id)
+                }
 
+            };
+            /*=========================================FIN METODOS PRIVADOS=====================================*/
+            
+            return this.each(function () {
+
+                var oSettings = options;
+
+                let method = {
+
+                    init: function () {
+                        _private.render.call(this,oSettings);
+                    }
+
+                };
+
+                method.init.call(this);
+
+            });
+            
+        }
+        
+    });    
+    
+})(jQuery);
+alert(8)
